9869410157d9fe8d9c7bece26617e17a1cf16a6b
Leguu <33807061+Leguu@users.noreply.github.com>
2023-11-06 13:48:58 -0500

    Add user seeding

--- src/domain/user/MySqlUserRepository.kt
+++ src/domain/user/MySqlUserRepository.kt
@@ -1,9 +1,6 @@
 package domain.user
 
-import infrastructure.InvalidOperation
-import infrastructure.MySqlDatabase
-import infrastructure.ResourceDatabaseTableInitializer
-import infrastructure.Unauthorized
+import infrastructure.*
 import jakarta.inject.Named
 import jakarta.inject.Singleton
 import java.sql.ResultSet
@@ -11,9 +8,37 @@ import java.sql.ResultSet
 @Named("MySqlUserRepository")
 @Singleton
 class MySqlUserRepository : UserRepository, ResourceDatabaseTableInitializer(listOf("users/UsersTableMigration")) {
+    init {
+        val allNames = sequence {
+            MySqlDatabase().use { db ->
+                val query = db.querySql("select name from Users;")
+                while (query.next()) {
+                    yield(query.getString("name"))
+                }
+            }
+        }
+
+        val lines = ClassLoaderUtil.getResourceContent("users.csv").lines()
+        for (line in lines) {
+            val parts = line.split(",")
+            val username = parts[0]
+            val password = parts[1]
+
+            if (password.isBlank()) continue
+
+            if (allNames.contains(username)) continue
+
+            try {
+                createUser(username, password)
+            } catch (_: Exception) {
+                continue
+            }
+        }
+    }
+
     override fun getUserById(id: Int): User? {
         MySqlDatabase().use { db ->
-            val results =  db.query("users/UserByIdQuery", id)
+            val results = db.query("users/UserByIdQuery", id)
             if (!results.next()) return null;
             return User.fromResultSet(results)
         }
